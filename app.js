7edf25bad2385af7624f881e742e8bbe0b2d5c76
Cloudkev <kvnrgt@yahoo.com>
2023-04-14 23:50:00 -0500

    first

---
+++ app.js
@@ -0,0 +1,15 @@
+//ES5 format
+
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+//ES2015
+
+const PI = 3.14;
+
+//Quiz
+//The difference between var and let is that you cannot redeclare let
+//the differentce between var and const is that you cannot redelcare or reassign const, but you can do both to var.
+// the difference between let and const is that you can reassign let, but you cannot do that to const. 
+//Variable hoisting is when using var, the variables get run first even if their value is not defined. so it will appear but will be set to undefined. 
+
